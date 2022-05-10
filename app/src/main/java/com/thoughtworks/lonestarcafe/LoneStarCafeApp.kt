039f4ef946cb2e8340896db51fec9ff14670591f
Siyang Li <syli@thoughtworks.com>
2022-05-10 23:28:24 +0800

    feat(compose): [Siyang] move scaffold into MenuScreen

--- app/src/main/java/com/thoughtworks/lonestarcafe/LoneStarCafeApp.kt
+++ app/src/main/java/com/thoughtworks/lonestarcafe/LoneStarCafeApp.kt
@@ -1,11 +1,7 @@
 package com.thoughtworks.lonestarcafe
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.res.stringResource
 import com.thoughtworks.lonestarcafe.data.AppCompositionRoot
 import com.thoughtworks.lonestarcafe.theme.LoneStarCafeTheme
 import com.thoughtworks.lonestarcafe.ui.MenuScreen
@@ -17,15 +13,6 @@ fun LoneStarCafeApp(compositionRoot: AppCompositionRoot) {
         val viewModel = remember {
             MenuScreenViewModel(compositionRoot.menuRepository)
         }
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = stringResource(id = R.string.app_name)) }
-                )
-            },
-        ) {
-
-            MenuScreen(viewModel)
-        }
+        MenuScreen(viewModel)
     }
 }
--- app/src/main/java/com/thoughtworks/lonestarcafe/ui/MenuScreen.kt
+++ app/src/main/java/com/thoughtworks/lonestarcafe/ui/MenuScreen.kt
@@ -5,23 +5,36 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.thoughtworks.lonestarcafe.R
 import com.thoughtworks.lonestarcafe.viewmodels.MenuScreenViewModel
 
 @Composable
 fun MenuScreen(
     menuScreenViewModel: MenuScreenViewModel
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .padding(start = 12.dp, end = 12.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        contentPadding = PaddingValues(top = 12.dp)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) }
+            )
+        },
     ) {
-        items(menuScreenViewModel.uiState.menuItems) {
-            MenuItem(menuItemUiState = it)
+        LazyColumn(
+            modifier = Modifier
+                .padding(start = 12.dp, end = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(top = 12.dp)
+        ) {
+            items(menuScreenViewModel.uiState.menuItems) {
+                MenuItem(menuItemUiState = it)
+            }
         }
     }
 }
